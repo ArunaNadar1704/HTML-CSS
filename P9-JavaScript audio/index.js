67eb603a6992771506b21aa0e256bdadda95221a
Aruna <nadararuna1704@gmail.com>
2022-07-09 00:25:29 +0530

    Added js-audio-page

---
+++ P9-JavaScript audio/index.js
@@ -0,0 +1,33 @@
+// const btn = document.querySelector(".btn");
+
+// const stopSounds= () => {
+//     btn.forEach((button) =>{
+//         const sound=document.getElementById(button.innerText);
+//         sound.pause();
+//         sound.currentTime =0;
+//     });
+// };
+
+// btn.forEach((button) => {
+//     button.addEventListener("click", () => {
+//         stopSounds();
+//         document.getElementById(button.innerText).play();
+// });
+// });
+
+const btn = document.querySelectorAll(".btn");
+
+const stopSounds = () => {
+  btn.forEach((button) => {
+    const sound = document.getElementById(button.innerText);
+    sound.pause();
+    sound.currentTime = 0;
+  });
+};
+
+btn.forEach((button) => {
+  button.addEventListener("click", () => {
+    stopSounds();
+    document.getElementById(button.innerText).play();
+  });
+});
